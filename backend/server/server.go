f476658a33ffb67550438301275c5ad40b9d951d
Sergey <xxsmotur@gmail.com>
2019-01-17 22:34:14 +0500

    Draft for post stats method

--- backend/server/server.go
+++ backend/server/server.go
@@ -11,9 +11,14 @@ func stats(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func postStats(w http.ResponseWriter, r *http.Request) {
+
+}
+
 // Create provides initialization of server
 func Create(c *Config) {
 	r := chi.NewRouter()
 	r.Get("/stats", stats)
+	r.Post("/stats", postStats)
 	http.ListenAndServe(c.Address, r)
 }
