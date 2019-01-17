a250a2795e981a7e5a774f766f0a0b02eec16188
Sergey <xxsmotur@gmail.com>
2019-01-17 23:11:32 +0500

    cmd: implementation of setup methods

--- backend/cmd/selitra/selitra.go
+++ backend/cmd/selitra/selitra.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+var flags = []cli.Flag{
+	cli.StringFlag{
+		EnvVar: "SELITRA_HOST",
+		Name:   "selitra-host",
+		Usage:  "host of selitra server",
+	},
+}
+
+func setupServer(c *cli.Context) {
+
+}
+func run(c *cli.Context) {
+	server, err := setupServer(c)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(provider)
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "selitra"
+	app.Action = run
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
+}
