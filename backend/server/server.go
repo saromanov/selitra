b330267fc52c7dc3f5a904e0c35dcc88c4f03388
Sergey <xxsmotur@gmail.com>
2019-01-19 23:18:47 +0500

    Using of the global app

--- backend/server/server.go
+++ backend/server/server.go
@@ -7,6 +7,8 @@ import (
 	"github.com/saromanov/selitra/backend/internal/app"
 )
 
+var a *app.App
+
 // stats returns current statistics
 func stats(w http.ResponseWriter, r *http.Request) {
 
@@ -18,6 +20,7 @@ func postStats(w http.ResponseWriter, r *http.Request) {
 
 // Create provides initialization of server
 func Create(a *app.App, c *Config) {
+	a = a
 	r := chi.NewRouter()
 	r.Get("/stats", stats)
 	r.Post("/stats", postStats)
