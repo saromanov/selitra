f0ea62093b739f293863e19105e943c67a53d95c
Sergey <xxsmotur@gmail.com>
2019-02-01 20:05:54 +0500

    add config struct

--- backend/cmd/selitra/selitra.go
+++ backend/cmd/selitra/selitra.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"os"
-
+	"io/ioutil"
 	"github.com/saromanov/selitra/backend/internal/app"
 	"github.com/saromanov/selitra/backend/server"
 	"github.com/urfave/cli"
+	"gopkg.in/yaml.v2"
 )
 
 // setupServer provides initialization of server
@@ -18,6 +19,21 @@ func setupServer(a *app.App, c *cli.Context) {
 func run(c *cli.Context) {
 	app := app.New()
 	setupServer(app, c)
+}
+
+// parseConfig provides parsing of the config .yml file
+func parseConfig(path string) (*alerting.Config, error) {
+	yamlFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open config file: %v", err)
+	}
+	var c *alerting.Config
+	err = yaml.Unmarshal(yamlFile, &c)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse .born.yml: %v", err)
+	}
+
+	return c, nil
 }
 
 func main() {
---
+++ backend/internal/structs/v1/config.go
@@ -0,0 +1,6 @@
+package v1
+
+// Config defines configuration for the app
+type Config struct {
+	Address string `yaml:"address"`
+}
