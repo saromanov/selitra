99a6fcbf81a7972006a802b17e1a08994c8dc4d1
Sergey <xxsmotur@gmail.com>
2019-02-04 19:13:43 +0500

    Additional fields at request

--- backend/server/request.go
+++ backend/server/request.go
@@ -2,9 +2,10 @@ package server
 
 // Request defines log payload
 type Request struct {
-	Level   string                 `json:"level"`
-	Message string                 `json:"message"`
-	Entries map[string]interface{} `json:"entries"`
-	Name    string                 `json:"name"`
-	Labels  []string               `json:"labels"`
+	Level   string   `json:"level"`
+	Message string   `json:"message"`
+	Name    string   `json:"name"`
+	Labels  []string `json:"labels"`
+	Entry   string   `json:"entry"`
+	Service string   `json:"service"`
 }
