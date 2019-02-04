cb83477d79ed93ad9896c2286369f089144c923e
Sergey <xxsmotur@gmail.com>
2019-02-04 18:32:30 +0500

    fixing of init attributes

--- backend/internal/structs/v1/config.go
+++ backend/internal/structs/v1/config.go
@@ -2,7 +2,7 @@ package v1
 
 // Config defines configuration for the app
 type Config struct {
-	Address    string `yaml:"address"`
-	DBName     string `yaml:"dbname`
-	DBPassword string `yaml:dbpassword"`
+	Address    string `yaml:"Address"`
+	DBName     string `yaml:"Dbname"`
+	DBPassword string `yaml:"Dbpassword"`
 }
