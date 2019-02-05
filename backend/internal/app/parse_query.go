a98b269c42ebc7cc98c76529f01ef4d3d2db0902
Sergey <xxsmotur@gmail.com>
2019-02-06 01:37:57 +0500

    queryParser: implementation of method for get date

--- backend/internal/app/parse_query.go
+++ backend/internal/app/parse_query.go
@@ -1,9 +1,17 @@
 package app
 
 import (
+	"errors"
 	"strings"
+	"github.com/jinzhu/now"
 	"github.com/saromanov/selitra/backend/internal/storage"
 )
+
+var (
+	errDateParse = errors.New("unable to parse date")
+	errDateRange = errors.New("unknown format of the date. It should be today, week, month, year")
+)
+
 // parseQuery provides parsing of the query and convert
 // it to the search request
 func parseQuery(query string)(*storage.SearchRequest, error) {
@@ -17,5 +25,14 @@ func parseQuery(query string)(*storage.SearchRequest, error) {
 // it should be in format 'date=today'
 func getDate(expr string) (int64, int64, error) {
 	values := strings.Split(expr, "=")
-	if len()
+	if len(values) <= 1 {
+		return 0, 0, errDateParse
+	}
+
+	switch values[1] {
+	case "today":
+		return now.BeginningOfDay().UnixNano(), now.EndOfDay().UnixNano(), nil
+	}
+
+	return 0, 0, errDateRange
 }
