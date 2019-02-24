c87a6762b00f0c2b40be2b018e22b10e02ea1bf2
Sergey <xxsmotur@gmail.com>
2019-02-24 15:23:42 +0500

    fix typo

--- backend/server/error.go
+++ backend/server/error.go
@@ -2,7 +2,7 @@ package server
 
 // Error defines the struct for response on the error case
 type Error struct {
-	StatusCoder int    `json:"status_code"`
-	Message     string `json:"message"`
-	Additional  string `json:"additional"`
+	StatusCode int    `json:"status_code"`
+	Message    string `json:"message"`
+	Additional string `json:"additional"`
 }
