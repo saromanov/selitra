75a0633412d460a3f838b4277d0a62fa1d2b8f89
Sergey <xxsmotur@gmail.com>
2019-02-24 15:25:59 +0500

    add method for make error

--- backend/server/error.go
+++ backend/server/error.go
@@ -1,8 +1,19 @@
 package server
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // Error defines the struct for response on the error case
 type Error struct {
 	StatusCode int    `json:"status_code"`
 	Message    string `json:"message"`
 	Additional string `json:"additional"`
+}
+
+func makeError(w http.ResponseWriter, e Error) {
+	res, _ := json.Marshal(e)
+	fmt.Fprint(w, res)
 }
